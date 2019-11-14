8a01733fe77070e8586b7fd8abdfeae6a51c14e6
Chima Ilo <collins.chima.ilo@gmail.com>
2019-11-14 15:25:44 +0000

    Auth setup

--- services/users/project/api/models.py
+++ services/users/project/api/models.py
@@ -1,6 +1,8 @@
+import datetime
+import jwt
+from flask import current_app
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.sql import func
-
-from werkzeug.security import generate_password_hash
 
 from project import db
 
@@ -21,11 +23,47 @@ class User(db.Model):
         self.email = email
         self.password = generate_password_hash(password)
 
-    # def set_password(self, password):
-    #     self.password_hash = generate_password_hash(password)
+    def __repr__(self):
+        return '<User: {}>'.format(self.username)
 
-    # def check_password(self, password):
-    #     return check_password_hash(self.password_hash, password)
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def encode_auth_token(self, user_id):
+        """Generates the auth token"""
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(
+                    days=current_app.config.get('TOKEN_EXPIRATION_DAYS'),
+                    seconds=current_app.config.get('TOKEN_EXPIRATION_SECONDS')
+                ),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                current_app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token - :param auth_token: - :return: integer|string
+        """
+        try:
+            payload = jwt.decode(
+                auth_token, current_app.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
 
     def to_json(self):
         return {
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -5,9 +5,11 @@ class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG_TB_ENABLED = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
+    TOKEN_EXPIRATION_DAYS = 30
+    TOKEN_EXPIRATION_SECONDS = 0
 
 
 class DevelopmentConfig(BaseConfig):
@@ -20,6 +22,8 @@ class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    TOKEN_EXPIRATION_DAYS = 0
+    TOKEN_EXPIRATION_SECONDS = 3
 
 
 class ProductionConfig(BaseConfig):
